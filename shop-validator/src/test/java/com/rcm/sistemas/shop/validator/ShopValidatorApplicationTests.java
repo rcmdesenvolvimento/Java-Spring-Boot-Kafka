f4dd568663e7d696893c44a8e3f6e5fa51bd0358
= <=>
2024-05-07 13:56:55 -0300

    criação do microservices shop-validator

---
+++ shop-validator/src/test/java/com/rcm/sistemas/shop/validator/ShopValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcm.sistemas.shop.validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
