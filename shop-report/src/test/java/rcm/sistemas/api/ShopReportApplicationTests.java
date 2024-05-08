7e0a3f8a8ebe0a269b5d890c25168adc0d7de482
= <=>
2024-05-08 17:03:39 -0300

    criação da api shop-report

---
+++ shop-report/src/test/java/rcm/sistemas/api/ShopReportApplicationTests.java
@@ -0,0 +1,13 @@
+package rcm.sistemas.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
